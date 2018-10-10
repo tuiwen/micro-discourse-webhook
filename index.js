a04802ed0d6968f1e3ff0830ba25c4afe70f977b
littledumb <jiamin@gmail.com>
2018-10-10 10:32:10 +0800

    initial checkin

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const crypto = require('crypto');
+const {send, text, json} = require('micro');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
+
+const webhook = data => {
+  console.log(`event type: ${data.discourseEventType}\n`);
+  console.log(`event: ${data.discourseEvent}\n`);
+  console.log('payload:\n');
+  console.log(data.payload);
+};
+
+module.exports = async (req, res) => {
+  // Validate the instance
+  const {headers} = req;
+  if (headers['x-discourse-instance'] !== process.env.DISCOURSE_INSTANCE) {
+    return send(res, 400, 'Invalid Discourse instance.\n');
+  }
+
+  // Validate the signature
+  const raw = await text(req);
+  const hmac = crypto.createHmac('sha256', process.env.DISCOURSE_WEBHOOK_SECRET);
+  const hash = `sha256=${hmac.update(raw).digest('hex')}`;
+  if (hash !== headers['x-discourse-event-signature']) {
+    return send(res, 401, 'Invalid signature.\n');
+  }
+
+  // Process the data
+  webhook({
+    discourseEventType: headers['x-discourse-event-type'],
+    discourseEvent: headers['x-discourse-event'],
+    payload: await json(req)
+  });
+
+  return send(res, 200);
+};
